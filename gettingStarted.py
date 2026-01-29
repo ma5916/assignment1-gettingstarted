a09609c6c95beba5ed4f9143159172f322695b50
Michael Ahn <michaelahn@mac.lan>
2026-01-28 20:44:03 -0800

    changed all logic and answers to the legit question answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,25 +10,25 @@ def welcome_assignment_answers(question):
             if question == ("In Slack, what is the secret passphrase posted in the"
                               " #lab-python-getting-started channel posted by a TA?"):
                 print(question)
-                answer = input()
+                answer = "pcap"
             elif question == "Are encoding and encryption the same? - Yes/No":
-                answer = input(question + " : ")
+                answer = "No"
             elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-                answer = input(question + " : ")
-            elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-                answer = input(question + " : ")
+                answer = "No"
+            elif question == "Is it possible to decode a message without a key? - Yes/No":
+                answer = "Yes"
             elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-                answer = input(question + " : ")
+                answer = "No"
             elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-                answer = input(question + " : ")
+                answer = "2116cd5906e14f85d4f4eb2a1b664cde27b5fb6e98096e364c5faf385d693fb4"
             elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-                answer = input(question + " : ")
+                answer = "No"
             elif question == ("What layer of the TCP/IP model does the protocol DNS belong to? - "
                               "The answer should be an integer number"):
-                answer = input(question + " : ")
+                answer = int(4)
             elif question == ("What layer of the TCP/IP model does the protocol ICMP belong to? - "
                               "The answer should be an integer number"):
-                answer = input(question + " : ")
+                answer = int(2)
             else:
                 ### you should understand why this else case should be included
                 ### what happens if there is a typo in one of the questions?
