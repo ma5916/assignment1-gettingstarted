f37b4eae6d11032cbb9f081d7edbc5c6d550cc8c
Michael Ahn <michaelahn@mac.lan>
2026-01-28 20:19:53 -0800

    added try except for EOFError

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,39 +3,43 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == ("In Slack, what is the secret passphrase posted in the"
-                      " #lab-python-getting-started channel posted by a TA?"):
-        print(question)
-        answer = input()
-    elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = input(question + " : ")
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = input(question + " : ")
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = input(question + " : ")
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = input(question + " : ")
-    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = input(question + " : ")
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = input(question + " : ")
-    elif question == ("What layer of the TCP/IP model does the protocol DNS belong to? - "
-                      "The answer should be an integer number"):
-        answer = input(question + " : ")
-    elif question == ("What layer of the TCP/IP model does the protocol ICMP belong to? - "
-                      "The answer should be an integer number"):
-        answer = input(question + " : ")
-    else:
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        # raise NameError('Question is Wrong')
-        answer = ("This is not my beautiful wife! This is not my beautiful car! "
-                  "How did I get here?")
-        raise ValueError(answer)
-    return answer
+    while True:
+        try:
+            #Students do not have to follow the skeleton for this assignment.
+            #Another way to implement is using a "case" statements similar to C.
+            if question == ("In Slack, what is the secret passphrase posted in the"
+                              " #lab-python-getting-started channel posted by a TA?"):
+                print(question)
+                answer = input()
+            elif question == "Are encoding and encryption the same? - Yes/No":
+                answer = input(question + " : ")
+            elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+                answer = input(question + " : ")
+            elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+                answer = input(question + " : ")
+            elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+                answer = input(question + " : ")
+            elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+                answer = input(question + " : ")
+            elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+                answer = input(question + " : ")
+            elif question == ("What layer of the TCP/IP model does the protocol DNS belong to? - "
+                              "The answer should be an integer number"):
+                answer = input(question + " : ")
+            elif question == ("What layer of the TCP/IP model does the protocol ICMP belong to? - "
+                              "The answer should be an integer number"):
+                answer = input(question + " : ")
+            else:
+                ### you should understand why this else case should be included
+                ### what happens if there is a typo in one of the questions?
+                ### maybe put something here to flag an issue and catch errors
+                # raise NameError('Question is Wrong')
+                answer = ("This is not my beautiful wife! This is not my beautiful car! "
+                          "How did I get here?")
+                raise ValueError(answer)
+        except EOFError:
+            pass
+        return answer
 # Complete all the questions.
 
 
